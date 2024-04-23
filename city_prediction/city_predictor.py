e94c701286dab3dcd26374b917049bf0ef354c7c
PasukkaBig <o_kristina2000@mail.ru>
2024-04-24 04:28:38 +0700

    implementing chain of thought

--- city_prediction/city_predictor.py
+++ city_prediction/city_predictor.py
@@ -22,28 +22,18 @@ class CityPredictor:
     def get_response(self, prompt_template: str, message: str) -> str:
         prompt = self.create_llm_prompt(prompt_template, message)
         response = self.llm.text_generation(
-            prompt, do_sample=False, max_new_tokens=30, stop_sequences=['.']).strip()
-        response = response.replace("Ответ: ", "").replace(".", "").replace('"', '')
-        prompt += response
-        return prompt, response
+            prompt, do_sample=False, max_new_tokens=50, stop_sequences=['.']).strip()
+        response = response.replace("Ответ: ", "").replace(".", "").replace('"', '').replace('(', '').replace(')', '')
+        return response
 
     def predict_city(self, message) -> None:
         self.llm = InferenceClient(model=self.model,
                                    timeout=8,
                                    token=self.huggingface_hub_token)
-        with open(self.prompt_template_path+'city_prediction.txt', 'r', encoding='utf-8') as f:
+        with open(self.prompt_template_path+'city_prediction_chain.txt', 'r', encoding='utf-8') as f:
             prompt_template = f.read().strip()
-        prompt, city = self.get_response(prompt_template, message)
-
-        print(f'[City 1]: {city}')
-        if city != 'None':
-            with open(self.prompt_template_path+'chain.txt', 'r', encoding='utf-8') as f:
-                prompt_template = f.read().strip()
-
-            prompt += prompt_template
-            
-            _, city = self.get_response(prompt, city)
-            print(f'[City 2]: {city}')
+        response = self.get_response(prompt_template, message)
+        city = response.split()[-1]
 
         return city
 
